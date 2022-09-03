2e01635457c3160e825d062058a7007ec1592a05
Majd <majd1996.1417@yahoo.com>
2022-09-03 23:50:58 +0300

    first commite in project

---
+++ script.js
@@ -0,0 +1,73 @@
+const form = document.getElementById('form');
+const username = document.getElementById('username');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const password2 = document.getElementById('password2');
+
+
+function ErrorMassege(input, message) {
+  const formControl = input.parentElement;
+  formControl.className = 'form-control error';
+  const small = formControl.querySelector('small');
+  small.innerText = message;
+}
+
+function SuccessMassege(input) {
+  const formControl = input.parentElement;
+  formControl.className = 'form-control success';
+}
+
+function checkEmail(input) {
+  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  if (re.test(input.value.trim())) {
+    SuccessMassege(input);
+  } else {
+    ErrorMassege(input, 'Email is not valid');
+  }
+}
+
+function checkRequired(inputArr) {
+  inputArr.forEach(function(input) {
+    if (input.value.trim() === '') {
+      ErrorMassege(input, `${getFieldName(input)} is required`);
+    } else {
+      SuccessMassege(input);
+    }
+  });
+}
+
+function checkLength(input, min, max) {
+  if (input.value.length < min) {
+    ErrorMassege(
+      input,
+      `${getFieldName(input)} must be at least ${min} characters`
+    );
+  } else if (input.value.length > max) {
+    ErrorMassege(
+      input,
+      `${getFieldName(input)} must be less than ${max} characters`
+    );
+  } else {
+    SuccessMassege(input);
+  }
+}
+
+function checkPasswordsMatch(input1, input2) {
+  if (input1.value !== input2.value) {
+    ErrorMassege(input2, 'Passwords do not match');
+  }
+}
+
+function getFieldName(input) {
+  return input.id.charAt(0).toUpperCase() + input.id.slice(1);
+}
+
+form.addEventListener('submit', function(e) {
+  e.preventDefault();
+
+  checkRequired([username, email, password, password2]);
+  checkLength(username, 3, 10);
+  checkLength(password, 6, 20);
+  checkEmail(email);
+  checkPasswordsMatch(password, password2);
+});
